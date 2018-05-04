ab2d30de3598d3c33d3f3b6d3e20701b6ade4eee
kmosunoff <kmosunoff@mail.ru>
2018-05-04 22:05:30 +0300

    Fields added

--- setting.php
+++ setting.php
@@ -12,6 +12,16 @@ echo 'Логин: <input id="login" type="text" name="login" value='.$data['logi
 echo "<p />";
 echo 'Почта: <input id="mail" type="text" name="mail" value='.$data['email'].' /><br />';
 echo "<p />";
+echo 'Имя: <input id="name" type="text" name="name" value='.$data['name'].' /><br />';
+echo "<p />";
+echo 'Фамилия: <input id="surname" type="text" name="surname" value='.$data['surname'].' /><br />';
+echo "<p />";
+echo 'Сайт компании: <input id="site" type="text" name="site" value='.$data['site'].' /><br />';
+echo "<p />";
+echo 'Телефон: <input id="phone" type="text" name="phone" value='.$data['phone'].' /><br />';
+echo "<p />";
+echo 'Пароль: <input id="password" type="text" name="password" value='.$data['password'].' /><br />';
+echo "<p />";
 echo '<input type="submit" name="Setting" value="Сохранить изменения"> </form>';
 
 ?>
